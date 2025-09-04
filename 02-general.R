332880c186c46c167d1912a35b6988515203f218
Alexandre Bohyn <alexandre.bohyn@kuleuven.be>
2025-09-04 10:34:05 +0200

    Use air code formatting

--- 02-general.R
+++ 02-general.R
@@ -6,10 +6,12 @@ library(cardinal)
 library(rtables.officer)
 
 ## table format (can be adapted)
-format_table <- c("mean_sd" = "xx.xx (xx.xx)",
-                  "range" = "xx.xx - xx.xx",
-                  "median" = "xx.xx",
-                  "count_fraction" = "xx (xx.xx%)")
+format_table <- c(
+  "mean_sd" = "xx.xx (xx.xx)",
+  "range" = "xx.xx - xx.xx",
+  "median" = "xx.xx",
+  "count_fraction" = "xx (xx.xx%)"
+)
 
 ## General demographics
 ## .. sex
@@ -20,19 +22,23 @@ format_table <- c("mean_sd" = "xx.xx (xx.xx)",
 
 ## create dataset
 df <- data[which(colnames(data) %in% c("D2", "D3", "D4", "D5", "D7"))]
-names(df) <- attributes(data)$variable.labels[which(colnames(data) %in% c("D2", "D3", "D4", "D5", "D7"))]
+names(df) <- attributes(data)$variable.labels[which(
+  colnames(data) %in% c("D2", "D3", "D4", "D5", "D7")
+)]
 df$Study <- factor("Need")
 
 ## calculate age
 df$`Quelle est votre année de naissance ?` <-
-  as.numeric(format(Sys.Date(), "%Y")) - df$`Quelle est votre année de naissance ?`
+  as.numeric(format(Sys.Date(), "%Y")) -
+  df$`Quelle est votre année de naissance ?`
 
 ## build basic table
 basic_table() %>%
   split_cols_by("Study") %>%
   analyze_vars(
-    names(df), .formats = format_table) %>%
+    names(df),
+    .formats = format_table
+  ) %>%
   add_colcounts() %>%
   add_overall_col("All") %>%
   build_table(df)
-
--- 03-eq5d.R
+++ 03-eq5d.R
@@ -11,22 +11,24 @@ col_today <- paste0("H", 7:11)
 
 data <- data %>%
   mutate(across(all_of(col_before), ~ as.integer(factor(.x)))) %>%
-  mutate(code_before = apply(select(., all_of(col_before)), 1, paste0, collapse = ""))
+  mutate(
+    code_before = apply(select(., all_of(col_before)), 1, paste0, collapse = "")
+  )
 
 data <- data %>%
   mutate(across(all_of(col_today), ~ as.integer(factor(.x)))) %>%
-  mutate(code_today = apply(select(., all_of(col_today)), 1, paste0, collapse = ""))
-
-data <- left_join(
-  data, 
-  select(value.set, state_string, value_before = value), 
-  by = c("code_before" = "state_string")
+  mutate(
+    code_today = apply(select(., all_of(col_today)), 1, paste0, collapse = "")
   )
 
 data <- left_join(
-  data, 
-  select(value.set, state_string, value_today = value), 
-  by = c("code_today" = "state_string")
+  data,
+  select(value.set, state_string, value_before = value),
+  by = c("code_before" = "state_string")
 )
 
-
+data <- left_join(
+  data,
+  select(value.set, state_string, value_today = value),
+  by = c("code_today" = "state_string")
+)
